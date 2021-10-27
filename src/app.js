17044369949fe021f8c9497f6dd581bc7ed7f265
Eren Genç <87229482+furkanerengenc@users.noreply.github.com>
2021-10-27 09:40:14 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,9 @@
+console.log("Merhaba kodlama io")
+
+let dolarDun = 9.20
+let dolarBugun = 9.30
+
+
+console.log(dolarDun)
+let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi", "Kamut Konut Kredisi"]
+console.log(konutKredileri)
